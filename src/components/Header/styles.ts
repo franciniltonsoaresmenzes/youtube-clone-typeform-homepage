111a4d75392bea80602c79874f7ba7d427d8fb91
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2022-08-01 10:09:24 -0400

    Finalizando UI Clone.

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+export const Container = styled(motion.header)`
+  width: 100%;
+  height: 200px;
+  background-color: rgba(0,0,0,.1);
+
+  position: fixed;
+  top: 0;
+  z-index: 999;
+  
+`;
+
